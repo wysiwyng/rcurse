73fe0317288cad70dc2c28498916873945251440
wysiwyng <wysiwyng@users.noreply.github.com>
2015-05-26 01:23:46 +0200

    add ai controller class

---
+++ src/AIControl.cpp
@@ -0,0 +1,22 @@
+/*
+ * AIControl.cpp
+ *
+ *  Created on: Apr 27, 2015
+ *      Author: wysiwyng
+ */
+
+#include "AIControl.h"
+
+AIControl::AIControl() {
+	// TODO Auto-generated constructor stub
+
+}
+
+AIControl::~AIControl() {
+	// TODO Auto-generated destructor stub
+}
+
+AIControl& AIControl::instance() {
+	static AIControl _instance;
+	return _instance;
+}
