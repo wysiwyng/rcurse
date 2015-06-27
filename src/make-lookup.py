364e3f35f60f2e9142823ce00a62a2fb908f0ccd
wysiwyng <wysiwyng@users.noreply.github.com>
2015-06-27 18:38:47 +0200

    added python script to generate python script from existing lookup.h

--- src/make-lookup.py
+++ src/make-lookup.py
@@ -1,32 +1,126 @@
-#generates the lookup table for rcurse map generation
-#TODO: needs to be adapted to current lookup.h
-
 f = open('lookup.h', 'w')
-f.write('static const char tiles[] = {\n')
-for i in range(0, 3400):
-    f.write('CHAR_WATER,\n')
-    
-for i in range(3400, 3700):
-    f.write('CHAR_EMPTY,\n')
-    
-for i in range(3700, 4500):
-    f.write('CHAR_GRASS,\n')
-    
-for i in range(4500, 5000):
-    f.write('CHAR_TALLGRASS,\n')
-    
-f.write('CHAR_TREASURE,\n')
+for i in range(1):
+    f.write("static const char tiles[] = {\n")
+for i in range(3400):
+    f.write("rcurse::CHAR_WATER,\n")
+for i in range(300):
+    f.write("rcurse::CHAR_EMPTY,\n")
+for i in range(388):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(10):
+    f.write("rcurse::CHAR_FLOWER,\n")
+for i in range(27):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(5):
+    f.write("rcurse::CHAR_FLOWER,\n")
+for i in range(52):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(8):
+    f.write("rcurse::CHAR_FLOWER,\n")
+for i in range(3):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_FLOWER,\n")
+for i in range(243):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_FLOWER,\n")
+for i in range(62):
+    f.write("rcurse::CHAR_GRASS,\n")
+for i in range(19):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(2):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(147):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(4):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(33):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(118):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(3):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(44):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(121):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREASURE,\n")
+for i in range(2):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(75):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(75):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(20):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(10):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(269):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(40):
+    f.write("rcurse::CHAR_TALLGRASS,\n")
+for i in range(57):
+    f.write("rcurse::CHAR_EMPTY,\n")
+for i in range(3):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(190):
+    f.write("rcurse::CHAR_EMPTY,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(109):
+    f.write("rcurse::CHAR_EMPTY,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(139):
+    f.write("rcurse::CHAR_EMPTY,\n")
+for i in range(186):
+    f.write("rcurse::CHAR_WALL,\n")
+for i in range(6):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(140):
+    f.write("rcurse::CHAR_WALL,\n")
+for i in range(1):
+    f.write("rcurse::CHAR_TREE,\n")
+for i in range(1667):
+    f.write("rcurse::CHAR_WALL,\n")
+for i in range(2000):
+    f.write("rcurse::CHAR_ICE,\n")
 
-for i in range(5001, 5500):
-    f.write('CHAR_TALLGRASS,\n')
-    
-for i in range(5500, 6000):
-    f.write('CHAR_EMPTY,\n')
-    
-for i in range(6000, 8000):
-    f.write('CHAR_WALL,\n')
-    
-for i in range(8000, 10000):
-    f.write('CHAR_ICE,\n')
-    
-f.write('};')
+f.write("};")
+f.close()
---
+++ src/make-make-lookup.py
@@ -0,0 +1,19 @@
+infile = open("lookup.h", "r")
+outfile = open("make-lookup.py", "w")
+
+old = infile.readline()
+count = 0
+outfile.write("f = open('lookup.h', 'w')\n")
+
+for line in infile:
+    count += 1
+    if old != line:
+        old = old.rstrip('\n')
+        outfile.write("for i in range({}):\n".format(count))
+        outfile.write("    f.write(\"" + old + "\\n\")\n")
+        count = 0
+    old = line
+    
+outfile.write("\nf.write(\"};\")\nf.close()")
+outfile.close()
+infile.close()
